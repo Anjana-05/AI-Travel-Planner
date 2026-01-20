c5a854dbdb811e2c5d42249df38345987ddf5c09
Anjana Baskaran <anjanabaskaran2005@gmail.com>
2026-01-20 22:26:57 +0530

    Backend updated

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,12 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://tripgen-ai.vercel.app/my-trips"
+  ]
+}));
 app.use(express.json());
 
 // Routes
